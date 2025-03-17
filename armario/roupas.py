adb4c6f867591693110d977ec76018eee9dab8f8
Daniel Luis <58662112+ddahnn@users.noreply.github.com>
2025-03-17 19:53:35 -0300

    roupas.py

--- armario/roupas.py
+++ armario/roupas.py
@@ -1,19 +1,40 @@
-class Roupas:
+class Roupa:
     '''Classe base para representar roupas'''
     def __init__(self):
-        pass  
+        pass
 
-class Camisa(Roupas):
+class Camisa(Roupa):
     '''Classe que representa uma camisa'''
-    def __init__(self, cor):
+    def __init__(self, cor, manga="Longa"):
         super().__init__()
         self.cor = cor
+        self.manga = manga
 
     def get_cor(self):
         '''Retorna a cor da camisa'''
         return f'Camisa {self.cor}'
 
-class Calça(Roupas):
+    def transformar_manga_curta(self):
+        self.manga = "Curta"
+        return self.manga
+
+    def virar_regata(self):
+        self.manga = "Regata"
+        return self.manga
+
+class CamisaCurta(Camisa):
+    def __init__(self, cor):
+        super().__init__(cor, "Curta")
+
+    def virar_regata(self):
+        self.manga = "Regata"
+        return self.manga
+
+class Regata(Camisa):
+    def __init__(self, cor):
+        super().__init__(cor, "Regata")
+
+class Calca(Roupa):
     '''Classe que representa uma calça'''
     def __init__(self, cor):
         super().__init__()
